cd663949c79fb679de321c0bb7acbe26e4c6f4aa
Erica <erica@ELU-MBPro.local>
2019-02-03 20:15:08 -0600

    leaflet & geoJSON assignment

--- 20190211_Leaflet_geoJSON/Level_1/static/js/logic.js
+++ 20190211_Leaflet_geoJSON/Level_1/static/js/logic.js
@@ -1,18 +1,14 @@
 // USGS Data
 var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-var platesJSON = "/static/js/PB2002_plates.json"
 
 // Perform a GET request to the query URL
 d3.json(URL, function(data) {
-    d3.json(platesJSON, function(plates) {
-        // Once we get a response, send the data.features object to the createFeatures function
-        createFeatures(data.features, plates.features)
-    })
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
 });
 
-
 // function that creates the circle markers and gets colors plus size based on magnitude
-function createFeatures(earthquakeData, platesData) {
+function createFeatures(earthquakeData) {
 
     var earthquakes = L.geoJSON(earthquakeData, {
         pointToLayer: function(feature, latlng) {
@@ -32,13 +28,8 @@ function createFeatures(earthquakeData, platesData) {
         }
     });
 
-    var tPlates = L.geoJSON(platesData, {
-            onEachFeature: function(feature, layer) {
-                layer.bindPopup("<h3>" + feature.properties.PlateName + "</h3>");
-            }
-        })
-        // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes, tPlates);
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
 };
 
 // function increases the size of the markers using a multiplier of 3
@@ -58,7 +49,7 @@ function getColor(d) {
         '#238443';
 };
 
-function createMap(earthquakesData, platesData) {
+function createMap(earthquakes) {
 
     // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -75,33 +66,15 @@ function createMap(earthquakesData, platesData) {
         accessToken: API_KEY
     });
 
-    var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "mapbox.outdoors",
-        accessToken: API_KEY
-    });
-
-    var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "mapbox.satellite",
-        accessToken: API_KEY
-    });
-
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
-        "Dark Map": darkmap,
-        "Outdoor Map": outdoors,
-        "Satellite": satellite,
-        "Street Map": streetmap
-
+        "Street Map": streetmap,
+        "Dark Map": darkmap
     };
 
     // Create overlay object to hold our overlay layer
     var overlayMaps = {
-        Earthquakes: earthquakesData,
-        Plates: platesData // level 2 add
+        Earthquakes: earthquakes
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -110,7 +83,7 @@ function createMap(earthquakesData, platesData) {
             37.09, -95.71
         ],
         zoom: 3,
-        layers: [streetmap, earthquakesData, platesData]
+        layers: [streetmap, earthquakes]
     });
 
     // Setting up the legend
